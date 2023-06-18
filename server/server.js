76139b0ab9685809a236651de29ee5f1362a5382
Golodhrim <holidayninjastaff@gmail.com>
2023-06-18 13:15:48 -0400

    build script added

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ app.get("/api/v1/blueberry", (req, res) => {
     res.status(200).send({message: "Shut up and give me food hooman!"});
 });
 
+// serve static assets
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
 app.listen(port, () => {
     console.log(`Server is up on: ${port}`);
 });
